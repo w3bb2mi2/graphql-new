2bfda097440a7eb540d27af9ec43721e2e7833b4
w3bb2mi2 <w3bb2mi2@gmail.com>
2022-07-26 17:59:47 +0300

    Дописал сервер

--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const {graphqlHTTP} = require("express-graphql")
 const cors = require('cors')
-
-
-
 const app = express()
 app.use(cors())
+const schema = require("./schema")
+const users = [{id: 1, username: "Georgiy", age: 25}]
+const createUser = (input) =>{
+    const id = Date.now()
+    return {
+        id, ...input
+    }
+}
+const root = {
+    getAllUsers: () => {
+        console.log(users)
+        return users
+    }, 
+    getUser: ({id})=>{
+        return users.find(user => user.id == id)
+    },
+    createUser: ({input})=>{
+        const user = createUser(input)
+        users.push(user)
+        console.log(users)
+        return user
+    }
+
+}
 
 app.use("/graphql", graphqlHTTP({
-    graphiql: true
+    graphiql: true, 
+    schema, 
+    rootValue: root
 }))
 
 app.listen(5000, () => console.log(`server has been started on port:`))
